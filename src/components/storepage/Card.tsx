7e5a06e82a6a3dd6686703f50ca5fd686f8102d5
hkoraven <hkoraven@uwaterloo.ca>
2023-04-24 01:41:32 -0400

    Initial implementation of card and store

--- src/components/storepage/Card.tsx
+++ src/components/storepage/Card.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import styles from "../../style"
 
-const Card = () => {
+type cardProps = {
+  id: string,
+  image: string,
+  title: string,
+  weight: string,
+  color: string,
+  price: string,
+}
+
+const Card = ({id, image, title, weight, color, price } : cardProps) => {
   return (
-    <div className=''>
-      <img />
-			  
+    <div className='flex flex-col'>
+      <img src={image} alt={id} />
+      <div className='flex flex-row'>
+        <div className='flex flex-col'>
+          <h2>{title}</h2>
+          <h1>{weight}</h1>
+          <h1>{color}</h1>
+        </div>
+        <h4>{price}</h4>
+      </div>			  
     </div>
   )
 }
--- src/components/storepage/Store.tsx
+++ src/components/storepage/Store.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { storeItems } from '../../data'
+import Card from './Card'
+import styles, { layout } from '../../style'
 
 const Store = () => {
   return (
-    <div>
-        Store
+    <div className={`${layout.section} bg-white`}>
+      <div className={`${styles.flexCenter} my-10`}>
+        <h6 className='font-sedgwick font-extrabold'>The Shop</h6>
+      </div>
+      <div className='flex flex-row flex-wrap'>
+        {storeItems.map((item, index) => (
+          <div key={index}>
+            <Card 
+              id={item.id} 
+              image={item.image} 
+              title={item.title} 
+              weight={item.weight} 
+              color={item.color} 
+              price={item.price} 
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
